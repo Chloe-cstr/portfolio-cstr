b43b43e816aa7dcbbeb5e4e38b24f4da9590e5de
Chloe Casteran <chloe.casteran@gmail.com>
2025-11-04 13:45:48 +0100

    blur on header

---
+++ src/component/CompetenceSection/CompetenceSection.jsx
@@ -0,0 +1,11 @@
+import "./competenceSection.scss";
+
+const CompetenceSection = () =>{
+    return(
+        <div className="competence">
+            <p>Hello World</p>
+        </div>
+    )
+};
+
+export default CompetenceSection;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import './home.scss';
 import Header from '../../component/Header/Header';
 import PresentationSection from '../../component/PresentationSection/PresentationSection';
+import CompetenceSection from '../../component/CompetenceSection/CompetenceSection';
 
 const Home = () => {
   return (
     <div>
         <Header />
         <PresentationSection />
+        <CompetenceSection />
     </div>
   );
 };
